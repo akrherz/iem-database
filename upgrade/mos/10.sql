49d7b90dd63aedd8eef5e5231ddee16201705dda
akrherz <akrherz@iastate.edu>
2020-09-28 11:05:52 -0500

    update MOS columns per NBM update
    see akrherz/pyIEM#282

---
+++ upgrade/mos/10.sql
@@ -0,0 +1,12 @@
+-- NBM MOS Updates
+ALTER TABLE alldata add txn smallint;
+ALTER TABLE alldata add xnd smallint;
+ALTER TABLE alldata add tsd smallint;
+ALTER TABLE alldata add dsd smallint;
+ALTER TABLE alldata add ssd smallint;
+ALTER TABLE alldata add gsd smallint;
+ALTER TABLE alldata add ifc smallint;
+ALTER TABLE alldata add ifv smallint;
+ALTER TABLE alldata add mvc smallint;
+ALTER TABLE alldata add mvv smallint;
+ALTER TABLE alldata add liv smallint;
