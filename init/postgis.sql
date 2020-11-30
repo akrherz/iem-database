0c47e0fe3860652e33bbe438138f584c13c1d12b
akrherz <akrherz@iastate.edu>
2020-11-29 22:26:17 -0600

    fix perms

--- init/postgis.sql
+++ init/postgis.sql
@@ -408,15 +408,14 @@ CREATE TABLE text_products (
     expire timestamptz,
     geom geometry(MultiPolygon, 4326)
 );
-
+ALTER TABLE text_products OWNER to mesonet;
+GRANT ALL on text_products to ldm;
 grant select on text_products to apache,nobody;
 
 create index text_products_idx  on text_products(product_id);
 CREATE INDEX text_products_issue_idx on text_products(issue);
 CREATE INDEX text_products_expire_idx on text_products(expire);
 create index text_products_pil_idx  on text_products(pil);
-
-GRANT SELECT on text_products to nobody,apache;
 
 ---
 --- riverpro
