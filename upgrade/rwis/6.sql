5375287e37ab9fd8574609bfdff238bd899e39eb
akrherz <akrherz@iastate.edu>
2020-12-02 20:24:35 -0600

    expand RWIS storage datatype

---
+++ upgrade/rwis/6.sql
@@ -0,0 +1,5 @@
+-- Wider storage
+ALTER TABLE alldata ALTER tfs0_text TYPE text;
+ALTER TABLE alldata ALTER tfs1_text TYPE text;
+ALTER TABLE alldata ALTER tfs2_text TYPE text;
+ALTER TABLE alldata ALTER tfs3_text TYPE text;
