3ff7e226fa8607a366f13d9dd8de00cfc23bed66
akrherz <akrherz@iastate.edu>
2021-03-05 14:19:31 -0600

    dedicated sps storage, text_products removed
    see akrherz/pyIEM#253

--- init/postgis.sql
+++ init/postgis.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION postgis;
 CREATE TABLE iem_schema_manager_version(
 	version int,
 	updated timestamptz);
-INSERT into iem_schema_manager_version values (47, now());
+INSERT into iem_schema_manager_version values (48, now());
 
 ---
 --- TABLES THAT ARE LOADED VIA shp2pgsql
@@ -413,26 +413,68 @@ CREATE TABLE vtec_missing_events(
 GRANT ALL on vtec_missing_events to mesonet,ldm;
 GRANT select on vtec_missing_events to nobody,apache;
 
----
---- Text Products
----
-CREATE TABLE text_products (
+-- Special Weather Statements
+CREATE TABLE sps(
+    product_id varchar(32),
     product text,
-    product_id character varying(32),
     pil char(6),
-    product_num smallint,
+    wfo char(3),
     issue timestamptz,
     expire timestamptz,
-    geom geometry(MultiPolygon, 4326)
-);
-ALTER TABLE text_products OWNER to mesonet;
-GRANT ALL on text_products to ldm;
-grant select on text_products to apache,nobody;
+    geom geometry(Polygon, 4326),
+    ugcs char(6)[],
+    landspout text,
+    waterspout text,
+    max_hail_size text,
+    max_wind_gust text,
+    tml_valid timestamp with time zone,
+    tml_direction smallint,
+    tml_sknt smallint,
+    tml_geom geometry(Point, 4326),
+    tml_geom_line geometry(Linestring, 4326)
+) PARTITION by range(issue);
+ALTER TABLE sps OWNER to mesonet;
+GRANT ALL on sps to ldm;
+GRANT SELECT on sps to apache,nobody;
 
-create index text_products_idx  on text_products(product_id);
-CREATE INDEX text_products_issue_idx on text_products(issue);
-CREATE INDEX text_products_expire_idx on text_products(expire);
-create index text_products_pil_idx  on text_products(pil);
+do
+$do$
+declare
+     year int;
+     mytable varchar;
+begin
+    for year in 2002..2030
+    loop
+        mytable := format($f$sps_%s$f$, year);
+        execute format($f$
+            create table %s partition of sps
+            for values from ('%s-01-01 00:00+00') to ('%s-01-01 00:00+00')
+            $f$, mytable, year, year + 1);
+        execute format($f$
+            ALTER TABLE %s OWNER to mesonet
+        $f$, mytable);
+        execute format($f$
+            GRANT ALL on %s to ldm
+        $f$, mytable);
+        execute format($f$
+            GRANT SELECT on %s to nobody,apache
+        $f$, mytable);
+        -- Indices
+        execute format($f$
+            CREATE INDEX %s_issue_idx on %s(issue)
+        $f$, mytable, mytable);
+        execute format($f$
+            CREATE INDEX %s_expire_idx on %s(expire)
+        $f$, mytable, mytable);
+        execute format($f$
+            CREATE INDEX %s_product_id_idx on %s(product_id)
+        $f$, mytable, mytable);
+        execute format($f$
+            CREATE INDEX %s_wfo_idx on %s(wfo)
+        $f$, mytable, mytable);
+    end loop;
+end;
+$do$;
 
 ---
 --- riverpro
--- upgrade/postgis/6.sql
+++ upgrade/postgis/6.sql
@@ -1,8 +1,2 @@
--- Storage of SPS issue and expire times
-ALTER TABLE text_products add issue timestamptz;
-ALTER TABLE text_products add expire timestamptz;
-
-CREATE INDEX text_products_issue_idx on text_products(issue);
-CREATE INDEX text_products_expire_idx on text_products(expire);
-
-GRANT SELECT on text_products to nobody,apache;
+-- text_products table removed
+SELECT 1;
