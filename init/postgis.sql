711a4e00164196676b8e0c122df6d6cf6111e570
akrherz <akrherz@iastate.edu>
2021-05-26 09:05:22 -0500

    add a different get_gid signature

--- init/postgis.sql
+++ init/postgis.sql
@@ -201,6 +201,16 @@ AS $_$
   (end_ts is null or end_ts > $2) and source = $3 LIMIT 1
 $_$;
 
+-- is_firewx version
+CREATE OR REPLACE FUNCTION get_gid(varchar, timestamptz, bool)
+RETURNS int
+LANGUAGE sql
+AS $_$
+  select gid from ugcs WHERE ugc = $1 and begin_ts <= $2 and
+  (end_ts is null or end_ts > $2) and
+  (case when $3 then source = 'fz' else source != 'fz' end) LIMIT 1
+$_$;
+
 
 ---
 --- Store IDOT dashcam stuff
--- upgrade/postgis/47.sql
+++ upgrade/postgis/47.sql
@@ -32,3 +32,13 @@ AS $_$
   select gid from ugcs WHERE ugc = $1 and begin_ts <= $2 and
   (end_ts is null or end_ts > $2) and source = $3 LIMIT 1
 $_$;
+
+CREATE OR REPLACE FUNCTION get_gid(varchar, timestamptz, bool)
+RETURNS int
+LANGUAGE sql
+AS $_$
+  select gid from ugcs WHERE ugc = $1 and begin_ts <= $2 and
+  (end_ts is null or end_ts > $2) and
+  (case when $3 then source = 'fz' else source != 'fz' end) LIMIT 1
+$_$;
+
