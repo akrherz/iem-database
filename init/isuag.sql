33c403a6057508433072b77497a025f3cf7bd0cd
akrherz <akrherz@iastate.edu>
2021-05-28 09:30:13 -0500

    add stations table to isuag database

--- init/isuag.sql
+++ init/isuag.sql
@@ -4,7 +4,55 @@
 CREATE TABLE iem_schema_manager_version(
 	version int,
 	updated timestamptz);
-INSERT into iem_schema_manager_version values (13, now());
+INSERT into iem_schema_manager_version values (14, now());
+
+CREATE EXTENSION postgis;
+
+-- synced from mesosite
+CREATE TABLE stations(
+	id varchar(20),
+	synop int,
+	name varchar(64),
+	state char(2),
+	country char(2),
+	elevation real,
+	network varchar(20),
+	online boolean,
+	params varchar(300),
+	county varchar(50),
+	plot_name varchar(64),
+	climate_site varchar(6),
+	remote_id int,
+	nwn_id int,
+	spri smallint,
+	wfo varchar(3),
+	archive_begin timestamptz,
+	archive_end timestamp with time zone,
+	modified timestamp with time zone,
+	tzname varchar(32),
+	iemid SERIAL,
+	metasite boolean,
+	sigstage_low real,
+	sigstage_action real,
+	sigstage_bankfull real,
+	sigstage_flood real,
+	sigstage_moderate real,
+	sigstage_major real,
+	sigstage_record real,
+	ugc_county char(6),
+	ugc_zone char(6),
+	ncdc81 varchar(11),
+    ncei91 varchar(11),
+	temp24_hour smallint,
+	precip24_hour smallint,
+    geom geometry(Point, 4326)
+);
+CREATE UNIQUE index stations_idx on stations(id, network);
+create UNIQUE index stations_iemid_idx on stations(iemid);
+GRANT SELECT on stations to apache,nobody;
+grant all on stations_iemid_seq to nobody,apache;
+GRANT ALL on stations to mesonet,ldm;
+GRANT ALL on stations_iemid_seq to mesonet,ldm;
 
 CREATE TABLE sm_daily (
   station char(5),
