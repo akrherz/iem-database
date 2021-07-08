c3c6596b8a11e30dd112865c1992c78248e96700
akrherz <akrherz@iastate.edu>
2021-07-08 09:46:36 -0500

    needs to be a left join

--- init/postgis.sql
+++ init/postgis.sql
@@ -1033,7 +1033,7 @@ GRANT SELECT on spc_outlook_geometries to nobody,apache;
 CREATE OR REPLACE VIEW spc_outlooks AS
     select id, issue, product_issue, expire, threshold, category, day,
     outlook_type, geom, product_id, updated, cycle
-    from spc_outlook o JOIN spc_outlook_geometries g
+    from spc_outlook o LEFT JOIN spc_outlook_geometries g
     on (o.id = g.spc_outlook_id);
 ALTER VIEW spc_outlooks OWNER to mesonet;
 GRANT SELECT on spc_outlooks to ldm,nobody,apache;
--- upgrade/postgis/55.sql
+++ upgrade/postgis/55.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE VIEW spc_outlooks AS
     select id, issue, product_issue, expire, threshold, category, day,
     outlook_type, geom, product_id, updated, cycle
-    from spc_outlook o JOIN spc_outlook_geometries g
+    from spc_outlook o LEFT JOIN spc_outlook_geometries g
     on (o.id = g.spc_outlook_id);
 ALTER VIEW spc_outlooks OWNER to mesonet;
 GRANT SELECT on spc_outlooks to ldm,nobody,apache;
