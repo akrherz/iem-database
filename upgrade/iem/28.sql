027dbaebcb5cc108b6cd4dd17f6acbfbfc8c4202
akrherz <akrherz@iastate.edu>
2021-09-27 11:30:30 -0500

    silently fix current_shef source column  (can't easily migrate)

--- upgrade/iem/28.sql
+++ upgrade/iem/28.sql
@@ -9,13 +9,13 @@ CREATE OR REPLACE RULE replace_current_shef AS ON
     INSERT TO current_shef WHERE (EXISTS 
         (SELECT 1 FROM current_shef WHERE
         station = new.station and physical_code = new.physical_code and
-        duration = new.duration and source = new.source and 
+        duration = new.duration and source = new.source and type = new.type and
         extremum = new.extremum and ((new.depth is null and depth is null) or 
         depth = new.depth))) DO INSTEAD 
         UPDATE current_shef SET value = new.value, valid = new.valid,
         dv_interval = new.dv_interval, qualifier = new.qualifier,
         unit_convention = new.unit_convention 
         WHERE station = new.station and physical_code = new.physical_code and
-        duration = new.duration and source = new.source and 
-        extremum = new.extremum and valid < new.valid and
+        duration = new.duration and source = new.source and
+        type = new.type and extremum = new.extremum and valid < new.valid and
         ((new.depth is null and depth is null) or depth = new.depth);
