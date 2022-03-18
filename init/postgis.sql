900b630a0e05e12302bea9bdf256d0435e505b3c
akrherz <akrherz@iastate.edu>
2022-03-18 08:38:44 -0500

    add storage for G-AIRMETs
    refs akrherz/pyIEM#566

--- init/postgis.sql
+++ init/postgis.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION postgis;
 CREATE TABLE iem_schema_manager_version(
 	version int,
 	updated timestamptz);
-INSERT into iem_schema_manager_version values (57, now());
+INSERT into iem_schema_manager_version values (58, now());
 
 ---
 --- TABLES THAT ARE LOADED VIA shp2pgsql
@@ -18,6 +18,40 @@ INSERT into iem_schema_manager_version values (57, now());
 ---   + tz
 ---   + uscounties
 ---   + warnings_import
+
+-- Storage of AIRMETs / Graphical AIRMET
+
+CREATE TABLE airmets(
+    label varchar(4) NOT NULL,
+    valid_from timestamptz,
+    valid_to timestamptz,
+    valid_at timestamptz,
+    product_id text,
+    hazard_type text,
+    weather_conditions text[],
+    status text,
+    geom geometry(Polygon, 4326)
+);
+CREATE INDEX airmets_idx on airmets(label, valid_at);
+CREATE INDEX airmets_geom_idx on airmets USING gist(geom);
+CREATE INDEX airmets_product_id_idx on airmets(product_id);
+ALTER TABLE airmets OWNER to mesonet;
+GRANT SELECT ON TABLE airmets TO nobody;
+GRANT ALL on TABLE airmets to ldm;
+
+-- Storage of Freezing Level found in AIRMETs
+
+CREATE TABLE airmet_freezing_levels(
+    valid_at timestamptz,
+    product_id text,
+    level int,
+    geom geometry(MultiLineString, 4326)
+);
+CREATE INDEX airmet_freezing_levels_idx on airmet_freezing_levels(valid_at);
+ALTER TABLE airmet_freezing_levels OWNER to mesonet;
+GRANT SELECT ON TABLE airmet_freezing_levels TO nobody;
+GRANT ALL on TABLE airmet_freezing_levels to ldm;
+
 
 -- Grid Population of the World
 -- https://sedac.ciesin.columbia.edu/data/set/gpw-v4-population-count-rev11/data-download
---
+++ upgrade/postgis/58.sql
@@ -0,0 +1,32 @@
+-- Storage of AIRMETs / Graphical AIRMET
+
+CREATE TABLE airmets(
+    label varchar(4) NOT NULL,
+    valid_from timestamptz,
+    valid_to timestamptz,
+    valid_at timestamptz,
+    product_id text,
+    hazard_type text,
+    weather_conditions text[],
+    status text,
+    geom geometry(Polygon, 4326)
+);
+CREATE INDEX airmets_idx on airmets(label, valid_at);
+CREATE INDEX airmets_geom_idx on airmets USING gist(geom);
+CREATE INDEX airmets_product_id_idx on airmets(product_id);
+ALTER TABLE airmets OWNER to mesonet;
+GRANT SELECT ON TABLE airmets TO nobody;
+GRANT ALL on TABLE airmets to ldm;
+
+-- Storage of Freezing Level found in AIRMETs
+
+CREATE TABLE airmet_freezing_levels(
+    valid_at timestamptz,
+    product_id text,
+    level int,
+    geom geometry(MultiLineString, 4326)
+);
+CREATE INDEX airmet_freezing_levels_idx on airmet_freezing_levels(valid_at);
+ALTER TABLE airmet_freezing_levels OWNER to mesonet;
+GRANT SELECT ON TABLE airmet_freezing_levels TO nobody;
+GRANT ALL on TABLE airmet_freezing_levels to ldm;
