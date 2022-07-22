5309db4fada0032e34bbebff56d33624fe380435
akrherz <akrherz@iastate.edu>
2022-07-22 09:16:56 -0500

    unversioned change of asos report_type labels
    see akrherz/iem#104

--- init/asos.sql
+++ init/asos.sql
@@ -94,8 +94,9 @@ GRANT SELECT on alldata_report_type to nobody;
 INSERT into alldata_report_type VALUES
         (0, 'Unknown'),
         (1, 'MADIS HFMETAR'),
-        (2, 'Routine'),
-        (3, 'Special');
+        (2, 'Legacy Routine+Special'),
+        (3, 'Routine'),
+        (4, 'Special');
 
 
 CREATE TABLE alldata(
--- upgrade/asos/3.sql
+++ upgrade/asos/3.sql
@@ -5,9 +5,10 @@ CREATE TABLE alldata_report_type(
 GRANT SELECT on alldata_report_type to nobody;
 
 INSERT into alldata_report_type VALUES
-	(0, 'Unknown'),
-	(1, 'MADIS HFMETAR'),
-	(2, 'Routine'),
-	(3, 'Special');
+    (0, 'Unknown'),
+    (1, 'MADIS HFMETAR'),
+    (2, 'Legacy Routine+Special'),
+    (3, 'Routine'),
+    (4, 'Special');
 
 ALTER TABLE alldata ADD report_type smallint REFERENCES alldata_report_type(id);
