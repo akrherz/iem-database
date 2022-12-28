fcd5e29c6280fe4014e0f1c3e5a647b1569c873e
akrherz <akrherz@iastate.edu>
2022-12-28 07:35:11 -0600

    add storage of WPC national max/min

--- init/iem.sql
+++ init/iem.sql
@@ -5,7 +5,24 @@ CREATE EXTENSION postgis;
 CREATE TABLE iem_schema_manager_version(
 	version int,
 	updated timestamptz);
-INSERT into iem_schema_manager_version values (31, now());
+INSERT into iem_schema_manager_version values (32, now());
+
+-- Storage of WPC national high low
+CREATE TABLE wpc_national_high_low(
+    product_id varchar(35),
+    station varchar(24),
+    state varchar(2),
+    name text,
+    date date,
+    sts timestamptz,
+    ets timestamptz,
+    n_x char(1),
+    value real
+);
+ALTER TABLE wpc_national_high_low OWNER to mesonet;
+GRANT ALL on wpc_national_high_low to ldm;
+GRANT SELECT on wpc_national_high_low to nobody;
+CREATE index wpc_national_high_low_date_idx on wpc_national_high_low(date);
 
 -- Storage of CF6 data
 CREATE TABLE cf6_data(
