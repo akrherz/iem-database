6409a64db16ae1eba46fe5038b2a57bda503a19b
akrherz <akrherz@iastate.edu>
2023-07-29 12:34:05 -0500

    add mastodon storage

--- init/mesosite.sql
+++ init/mesosite.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION postgis;
 CREATE TABLE iem_schema_manager_version(
     version int,
     updated timestamptz);
-INSERT into iem_schema_manager_version values (25, now());
+INSERT into iem_schema_manager_version values (26, now());
 
 --- ==== TABLES TO investigate deleting
 --- counties
@@ -37,6 +37,30 @@ CREATE TABLE archive_products(
     time_offset int,
     avail_lag int);
 GRANT SELECT on archive_products to nobody;
+
+create table iembot_mastodon_apps(
+    id serial unique not null,
+    server text unique not null,
+    created timestamptz default now(),
+    updated timestamptz default now(),
+    client_id text not null,
+    client_secret text not null
+);
+alter table iembot_mastodon_apps owner to mesonet;
+grant all on iembot_mastodon_apps to nobody;
+
+create table iembot_mastodon_oauth(
+    appid int references iembot_mastodon_apps(id) not null,
+    screen_name text not null,
+    created timestamptz default now(),
+    updated timestamptz default now(),
+    password text,
+    access_token text,
+    iem_owned bool default 'f',
+    disabled bool default 'f'
+);
+alter table iembot_mastodon_oauth owner to mesonet;
+grant all on iembot_mastodon_oauth to nobody;
 
 -- Mostly for slack at the moment
 CREATE TABLE iembot_webhooks(
