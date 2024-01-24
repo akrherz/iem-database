bf7eb322047095532ecb052aa2b024f40c313d04
akrherz <akrherz@iastate.edu>
2024-01-24 12:22:02 -0600

    add era5land,nldas soil vars to coop
    refs akrherz/iem#595
    refs akrherz/iem#321

---
+++ upgrade/coop/19.sql
@@ -0,0 +1,4 @@
+alter table alldata add era5land_soilt4_avg real;
+alter table alldata add era5land_soilm4_avg real;
+alter table alldata add nldas_soilt4_avg real;
+alter table alldata add nldas_soilm4_avg real;
