a24a0a75bd98bc4bf52ad3e230e4f81f29febd78
akrherz <akrherz@iastate.edu>
2025-05-13 14:37:39 -0500

    🎨 Rework SIGMETs storage

--- README.md
+++ README.md
@@ -106,7 +106,6 @@ postgis__airmets.sql | Some AIRMETs from 10 Aug 2024
 postgis__cwas.sql | Some Center Weather Advisories from 10 Aug 2024
 postgis__fema_regions.sql.gz | Simplified FEMA Regions
 postgis__sigmets_archive.sql | Some SIGMETs from 10 Aug 2024
-postgis__sigmets_current.sql | Some current SIGMETs to get goosed in CI
 postgis__spc_outlook_geometries.sql.gz | SPC/WPC Outlooks 1-8 Aug 2024
 postgis__sps2024.sql.gz | SPSs from Aug 2024
 postgis_lsrs.sql | DMX LSRs from 2018-06-20,2018-06-21,2024-05-21, One BGM LSR from 2023
--- init/postgis.sql
+++ init/postgis.sql
@@ -10,7 +10,7 @@ insert into spatial_ref_sys
 CREATE TABLE iem_schema_manager_version(
     version int,
     updated timestamptz);
-INSERT into iem_schema_manager_version values (73, now());
+INSERT into iem_schema_manager_version values (74, now());
 
 ---
 --- TABLES THAT ARE LOADED VIA shp2pgsql
@@ -918,34 +918,46 @@ CREATE TABLE nws_ugc (
 
 grant select on nws_ugc to nobody;
 
----
---- SIGMET Convective Outlook
----
-CREATE TABLE sigmets_current(
-    sigmet_type char(1),
-    label varchar(16),
-    issue timestamp with time zone,
-    expire timestamp with time zone,
-    raw text, -- removeme after pyiem release
-    product_id varchar(36)
-);
-ALTER TABLE sigmets_current OWNER to mesonet;
-GRANT ALL on sigmets_current to ldm;
-SELECT AddGeometryColumn('sigmets_current', 'geom', 4326, 'POLYGON', 2);
-GRANT SELECT on sigmets_current to nobody;
+-- SIGMETs
+create table alldata_sigmets(
+    sigmet_type char(1) not null,
+    label varchar(16) not null,
+    issue timestamp with time zone not null,
+    expire timestamp with time zone not null,
+    product_id varchar(36) not null,
+    geom geometry(Polygon, 4326) not null,
+    narrative text,
+) partition by range(issue);
+alter table alldata_sigmets owner to mesonet;
+grant all on alldata_sigmets to ldm;
+grant select on alldata_sigmets to nobody;
+create index alldata_sigmets_idx on alldata_sigmets(issue);
 
-CREATE TABLE sigmets_archive(
-    sigmet_type char(1),
-    label varchar(16),
-    issue timestamp with time zone,
-    expire timestamp with time zone,
-    raw text, -- removeme after pyiem release
-    product_id varchar(36)
-);
-SELECT AddGeometryColumn('sigmets_archive', 'geom', 4326, 'POLYGON', 2);
-alter table sigmets_archive owner to mesonet;
-grant all on sigmets_archive to ldm;
-GRANT SELECT on sigmets_archive to nobody;
+do
+$do$
+declare
+     year int;
+     mytable varchar;
+begin
+    for year in 2005..2030
+    loop
+        mytable := format($f$sigmets_%s$f$, year);
+        execute format($f$
+            create table %s partition of alldata_sigmets
+            for values from ('%s-01-01 00:00+00') to ('%s-01-01 00:00+00')
+            $f$, mytable, year, year + 1);
+        execute format($f$
+            ALTER TABLE %s OWNER to mesonet
+        $f$, mytable);
+        execute format($f$
+            GRANT ALL on %s to ldm
+        $f$, mytable);
+        execute format($f$
+            GRANT SELECT on %s to nobody
+        $f$, mytable);
+    end loop;
+end;
+$do$;
 
 ---
 --- NEXRAD N0R Composites 
